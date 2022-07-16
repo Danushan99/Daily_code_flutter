31a1bc2a44269b2e3f738e920776f99845d88719
Danushan Ravendran <it20051648@my.sliit.lk>
2022-07-16 12:48:46 +0530

    3nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
+import 'package:daily_code/secdart.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,26 +11,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      // Initially display FirstPage
+      home: FirstPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
       body: Center(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text("Screen"),
-      ],
-    ),)
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'First Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            ElevatedButton(
+              child: Text('Go to second'),
+              onPressed: () {
+                // Pushing a route directly, WITHOUT using a named route
+                Navigator.of(context).push(
+                  // With MaterialPageRoute, you can pass data between pages,
+                  // but if you have a more complex app, you will quickly get lost.
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        SecondPage(data: 'Hello there from the first page!'),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
     );
-
   }
 }
-
